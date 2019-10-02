73b2579ed410fb26a6632d3da90ebb7ec33bd5da
Jason Allen <Jason94Allen@hotmail.com>
2019-10-02 16:12:12 -0400

    Deleted all files within this branch to start fresh.

--- !Magic Unity Project/Assets/!Magic/Scripts/FailParticle.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FailParticle : MonoBehaviour
-{
-	public ParticleSystem m_FailParticle;
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-# !Magic
-### A Futuristic VR Spellcasting Game Utilizing Gesture Recognition
-##### Using Unity 2019.1.8f1
