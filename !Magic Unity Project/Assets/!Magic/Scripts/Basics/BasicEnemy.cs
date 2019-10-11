91ba52df560c2524a0236179041945c701c94796
Samuel Paquette <paqu0363@algonquinlive.com>
2019-10-11 12:21:44 -0400

    Basic Enemy member variables added

--- !Magic Unity Project/Assets/!Magic/Scripts/Basics/BasicEnemy.cs
+++ !Magic Unity Project/Assets/!Magic/Scripts/Basics/BasicEnemy.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class BasicEnemy : ASpellUser
 {
+    Player m_playerRef;
+    Vector3 m_playerPos;
+    float m_playerHealth;
+    float m_enemyHealth;
+
+    void Awake()
+    {
+        m_playerRef = GameObject.FindGameObjectWithTag("Player");
+        m_playerPos = m_playerRef.transform.position;
+        m_playerHealth = m_playerRef.HealthCurrent.Get();
+    }
+
     public override Transform Aiming => throw new System.NotImplementedException();
 }
