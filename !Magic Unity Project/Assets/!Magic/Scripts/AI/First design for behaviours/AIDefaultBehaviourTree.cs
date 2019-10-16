ac17ccdcf1631998705636233ab8e5664a47c902
Samuel Paquette <paqu0363@algonquinlive.com>
2019-10-16 15:09:15 -0400

    AI Behaviour tree push uncurrupted

---
+++ !Magic Unity Project/Assets/!Magic/Scripts/AI/First design for behaviours/AIDefaultBehaviourTree.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AIData))]
+public class AIDefaultBehaviourTree : MonoBehaviour
+{
+    private AIData data;
+    private AIFollow followScript;
+    private Idle idleScript;
+    private Look lookScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //check if behaviour tree has all default behaviours.
+        if (GetComponent<AIFollow>() == null)
+            gameObject.AddComponent<AIFollow>();
+
+        if (GetComponent<Idle>() == null)
+            gameObject.AddComponent<Idle>();
+
+        if (GetComponent<Look>() == null)
+            gameObject.AddComponent<Look>();
+
+        //set our scripts.
+        data = GetComponent<AIData>();
+        followScript = GetComponent<AIFollow>();
+        idleScript = GetComponent<Idle>();
+        lookScript = GetComponent<Look>();
+
+        //set our current state dependant on the Ai's current behaviour.
+        switch (data.currentBehaviour)
+        {
+            case AIData.Behaviour.Follow:
+                data.state = followScript.OnBehaviourStart;
+                break;
+
+            //case AIData.Behaviour.Idle:
+            //    data.state = idleScript.OnBehaviourStart;
+            //    break;
+
+            //case AIData.Behaviour.Look:
+            //    data.state = lookScript.OnBehaviourStart;
+            //    break;
+
+            default:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if current behaviour is changed from last frame.
+        if(data.currentBehaviour != data.lastBehaviour)
+        {
+            //set the state according to its new behaviour.
+            switch (data.currentBehaviour)
+            {
+                case AIData.Behaviour.Follow:
+                    data.state = followScript.FollowFunction;
+                    break;
+
+                //case AIData.Behaviour.Idle:
+                //    data.state = idleScript.OnBehaviourStart;
+                //    break;
+
+                //case AIData.Behaviour.Look:
+                //    data.state = lookScript.OnBehaviourStart;
+                //    break;
+
+                default:
+                    break;
+            }
+
+            //stop the last behaviour.
+            switch (data.lastBehaviour)
+            {
+                case AIData.Behaviour.Follow:
+                    followScript.OnBehaviourEnd();
+                    break;
+
+                //case AIData.Behaviour.Idle:
+                //    idleScript.OnBehaviourEnd();
+                //    break;
+
+                //case AIData.Behaviour.Look:
+                //    lookScript.OnBehaviourEnd();
+                //    break;
+
+                default:
+                    break;
+            }
+        }
+
+        //run the functionality for this frame.
+        data.state();
+        //update last behaviour. 
+        data.lastBehaviour = data.currentBehaviour;
+    }
+}
---
+++ !Magic Unity Project/Assets/!Magic/Scripts/AI/First design for behaviours/AIFollow.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIFollow : MonoBehaviour
+{
+    private AIData data;
+    private float pathUpdateTimer;
+    //delay to only update the follow point after the delay amount.
+    private float pathUpdateDelay = 1.0f;
+    private float minDistanceToPoint = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        data = GetComponent<AIData>();
+    }
+
+    public void OnBehaviourStart()
+    {
+        if(Vector3.SqrMagnitude(data.followObject.position - data.AItransform.position) > data.maxMoveCushion * data.maxMoveCushion)
+        {
+            data.agent.SetDestination(data.followObject.position);
+            pathUpdateTimer = 0;
+            data.state = FollowFunction;
+        }
+        else
+        {
+            data.state = IdleFunction;
+            //replace with data.IdleScript.OnBehaviourStart();
+        }
+    }
+
+    public void OnBehaviourEnd()
+    {
+        //empty for now.
+    }
+
+    public void FollowFunction()
+    {
+        pathUpdateTimer += Time.deltaTime;
+
+        //if too close to me, go to idle, or in the future, go to attack and change min distanceToPoint to the AI's attack range.
+        if(Vector3.SqrMagnitude(data.followObject.position - data.AItransform.position) < minDistanceToPoint * minDistanceToPoint)
+        {
+            data.state = IdleFunction;
+            //replace with data.IdleScript.OnBehaviourStart();
+        }
+        else if(pathUpdateTimer > pathUpdateDelay)
+        {
+            data.agent.SetDestination(data.followObject.position);
+            pathUpdateTimer = 0;
+        }
+    }
+
+    public void IdleFunction()
+    {
+        //if distance between AI and follow object is smaller than the max dist that we want,
+        if (Vector3.SqrMagnitude(data.followObject.position - data.AItransform.position) > data.maxMoveCushion * data.maxMoveCushion)
+        {
+            data.agent.SetDestination(data.followObject.position);
+            data.state = FollowFunction;
+        }
+    }
+}
