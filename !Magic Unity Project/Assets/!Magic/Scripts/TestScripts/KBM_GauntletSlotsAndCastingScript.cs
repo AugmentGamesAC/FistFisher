754c5b19640d96987ec47afed1a9296f9a47c8b9
Eric Krushelniski <krus0021@algonquinlive.com>
2019-10-28 15:52:31 -0400

    more fiddling with interactables stuff

---
+++ !Magic Unity Project/Assets/!Magic/Scripts/TestScripts/KBM_GauntletSlotsAndCastingScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KBM_GauntletSlotsAndCastingScript : MonoBehaviour
+{
+    public KeyCode m_castOrAssignToSlot1 = KeyCode.Alpha1;
+    public KeyCode m_castOrAssignToSlot2 = KeyCode.Alpha2;
+    public KeyCode m_castOrAssignToSlot3 = KeyCode.Alpha3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (Input.GetKeyDown(m_castOrAssignToSlot1))
+        {
+        }
+
+        if (Input.GetKeyDown(m_castOrAssignToSlot2))
+        {
+        }
+
+        if (Input.GetKeyDown(m_castOrAssignToSlot3))
+        {
+        }
+
+
+
+
+    }
+}
