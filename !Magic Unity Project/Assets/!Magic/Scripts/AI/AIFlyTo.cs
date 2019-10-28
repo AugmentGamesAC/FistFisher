847c4537a09acc32f21fe41e824e1cc688a9978b
Eric Krushelniski <krus0021@algonquinlive.com>
2019-10-28 01:13:16 -0400

    I was vastly overcomplicating it. Flyto now much less costly. Still issues with collision and integrating with other movements

--- !Magic Unity Project/Assets/!Magic/Scripts/AI/AIFlyTo.cs
+++ !Magic Unity Project/Assets/!Magic/Scripts/AI/AIFlyTo.cs
@@ -8,7 +8,7 @@ public class AIFlyTo : ABehaviour
     public float m_ObstacleDetectRadius = 5.0f;
     public float m_avoidanceWeight = 50.5f;
 
-    bool m_isMoving = false;
+   // bool m_isMoving = false;
 
     Vector3 m_moveDirection = Vector3.zero;
 
@@ -33,21 +33,16 @@ public class AIFlyTo : ABehaviour
         }
     }
 
-    //since we're not doing navmeshes for this, we need to use lateupdate to actually move and the other update to get direction
-    //(Sam is using Update for all AI behaviour, so this should be fine)
     void LateUpdate()
     {
-        if(m_isMoving)
+        gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, Quaternion.LookRotation(m_moveDirection), m_data.m_angularSpeed * Time.deltaTime);
+        if (Vector3.Distance(gameObject.transform.position, m_data.followObject.transform.position) < m_data.m_movementSpeed * Time.deltaTime)
         {
-            gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, Quaternion.LookRotation(m_moveDirection), m_data.m_angularSpeed * Time.deltaTime);
-            if (Vector3.Distance(gameObject.transform.position, m_data.followObject.transform.position) < m_data.m_movementSpeed * Time.deltaTime)
-            {
-                gameObject.transform.position = m_data.followObject.transform.position;
-            }
-            else
-            {
-                gameObject.transform.Translate(0, 0, m_data.m_movementSpeed * Time.deltaTime);
-            }
+            gameObject.transform.position = m_data.followObject.transform.position;
+        }
+        else
+        {
+            gameObject.transform.Translate(0, 0, m_data.m_movementSpeed * Time.deltaTime);
         }
     }
 
@@ -67,54 +62,42 @@ public class AIFlyTo : ABehaviour
         }
         else if (m_updateTimer > m_updateDelay && PlayerInLineOfSight())
         {
-            m_isMoving = true;
             m_moveDirection = GetDirectionToMove();
+
+
+
             m_updateTimer = 0;
         }
 
         //LateUpdate();
     }
 
-
-    //spherecast to see if it collides with anything
-    private GameObject[] SpherecastForCollidables()
+    //shoot ray towards movement direction, if it hits, get a reflection
+    private Vector3 CollisionAvoidance(Vector3 targetdir)
     {
+        Vector3 aviodanceDir = Vector3.zero;
         int Mask = ~LayerMask.GetMask("Ignore Raycast");
-        RaycastHit[] hitInfos = Physics.SphereCastAll(gameObject.transform.position, m_ObstacleDetectRadius, Vector3.down, 0.1f, Mask);
-        GameObject[] NearbyObjects = new GameObject[hitInfos.Length];
 
-        for (int i=0;i< hitInfos.Length;i++)
+        RaycastHit hitInfo = new RaycastHit();
+        if(Physics.Raycast(gameObject.transform.position, targetdir, out hitInfo, m_ObstacleDetectRadius, Mask))
         {
-            Collider c = hitInfos[i].transform.gameObject.GetComponent<Collider>();
-            if (c != null && c.isTrigger==false) //this will be null if there is no gameobject, is no collider, or collider is only a trigger
-            {
-                NearbyObjects[i] = hitInfos[i].transform.gameObject;
-            }
+            aviodanceDir = Vector3.Reflect(targetdir, hitInfo.normal);
         }
-        return NearbyObjects;
+
+
+        return aviodanceDir;
     }
 
-    //gets a spherecast of all nearby objects, tries to avoid them
+    //gets a diretion to move by a durect line towards target, then an avoidance if going to hit something
     Vector3 GetDirectionToMove()
     {
-
-        GameObject[] NearbyObjects = SpherecastForCollidables(); //get list of all nearby objects that can be collided with
-        Vector3 avoidance = Vector3.zero;
         Vector3 direction = Vector3.zero;
 
-        if (NearbyObjects != null)
-        {
-            foreach (GameObject g in NearbyObjects)
-            {
-                //if (Vector3.Distance(g.transform.position, transform.position) < m_ObstacleDetectRadius) //we'll just assume detection radius is avoidance radius as well to save some calcs
-                {
-                    avoidance += (transform.position - g.transform.position);
-                }
-            }
-        }
 
-        Vector3 TargetDir = m_data.followObject.position - transform.position;
-        direction = TargetDir + avoidance * m_avoidanceWeight;
+        direction = m_data.followObject.position - transform.position; 
+        direction = Vector3.Normalize(direction);
+
+        direction += CollisionAvoidance(direction) * m_avoidanceWeight;
         direction = Vector3.Normalize(direction);
 
         return direction;
