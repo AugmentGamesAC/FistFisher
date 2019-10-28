a4e02cb2ea9cc541896cebfb403fbcbec8fd9f3a
Brian <ande0246@algonquinlive.com>
2019-10-28 13:50:15 -0400

    Work with Sam on spell clean up

--- !Magic Unity Project/Assets/!Magic/Scripts/Spells/SpellInstance.cs
+++ !Magic Unity Project/Assets/!Magic/Scripts/Spells/SpellInstance.cs
@@ -21,11 +21,13 @@ public class SpellInstance : MonoBehaviour
     public InstanceStates InstantceState { get { return m_InstanceState; } }
 
 
+
+
     public Spell m_Spell;
     /// <summary>
     /// way to ensure that instant damage is not reapplied
     /// </summary>
-    protected Dictionary<ASpellUser, float> DamageTimingResolution;
+    protected Dictionary<ASpellUser, float> m_CooldownList;
 
     public void UpdateMaterial(Spell.Elements elementalEffect)
     {
@@ -39,12 +41,66 @@ public class SpellInstance : MonoBehaviour
         m_InstanceState = state;
     }
 
+    private void FixedUpdate()
+    {
+        ASpellUser[] keys = new List<ASpellUser>(m_CooldownList.Keys).ToArray();
+
+        foreach (ASpellUser damaged in keys)
+        {
+            m_CooldownList[damaged] -= Time.deltaTime;
+            if (m_CooldownList[damaged] <= 0)
+                m_CooldownList.Remove(damaged);
+        }
+        ResolveProjectileBehavoir();
+        ResolveExplosionBehavoir();
+    }
+
+    //TODO: some logic for how exploision is to be handled.
+    private void ResolveExplosionBehavoir()
+    {
+        
+    }
+
+
+    //TODO: MISSING Progectile movement should be call in fixedUpdate
+    private void ResolveProjectileBehavoir()
+    {
+        if (!m_Spell.Description.Effect.HasFlag(SpellDescription.Effects.Projectile))
+            return;
+        throw new System.NotImplementedException();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if there is a contact explosion code goes here
+        throw new System.NotImplementedException();
+    }
+
+    private void ResolveExplosion()
+    {
+
+    }
+
+
     private void OnTriggerStay(Collider other)
     {
         if (!((m_InstanceState == InstanceStates.IsAiming) || (m_InstanceState == InstanceStates.IsEviromental)))
             return;
 
+        ASpellUser otherSpellUser = (ASpellUser)other.gameObject.GetComponent(typeof(ASpellUser));
 
+        if (otherSpellUser == null)
+            return;
+
+        GameObject otherEnemy = other.gameObject;
+
+        if (!m_CooldownList.ContainsKey(otherSpellUser))
+        {
+            otherSpellUser.TakeDamage(m_Spell.Damage);
+            if (!otherSpellUser.IsDead())
+                m_CooldownList.Add(otherSpellUser, m_Spell.Cooldown);
+            return;
+        }
 
     }
 }
