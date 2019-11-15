fc7775afbb251a1ab30b591b9009ed7a13e4419d
EK <krus0021@algonquinlive.com>
2019-11-15 09:44:04 -0500

    more wip

--- Fist Fishing/Assets/Scripts/Fish/BasicFish.cs
+++ Fist Fishing/Assets/Scripts/Fish/BasicFish.cs
@@ -11,10 +11,13 @@ public class BasicFish : MonoBehaviour
     protected FishArchetype m_fishArchetype;
     public FishArchetype FishType { get { return m_fishArchetype; } }
 
-    [SerializeField]
     public class FishyPart : InspectorDictionary<Collider, float> { }
     [SerializeField]
-    public FishyPart m_hitBoxModifiers = new FishyPart();
+    protected FishyPart m_hitBoxModifiers;
+
+    private Camera m_camera;
+    public TargetController m_targetController;//don't set this manually.
+    public bool m_isListed;
 
     //behaviour
     protected BehaviorTree m_behaviour;
@@ -22,16 +25,45 @@ public class BasicFish : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        m_camera = Camera.main;
+        m_isListed = false;
+
         //Get all hitboxes from each fish compound colliders.
-        /*FishHitBox[] m_hitBoxes = GetComponentsInChildren<FishHitBox>();
+        FishHitBox[] m_hitBoxes = GetComponentsInChildren<FishHitBox>();
 
         if (m_hitBoxes != null)
         {
             //add them to our InspectorDictionary.
             foreach (FishHitBox hitBox in m_hitBoxes)
             {
-                m_hitBoxModifiers.Add(hitBox.m_HitBoxModifier.Key, hitBox.m_HitBoxModifier.Value);
+                //m_hitBoxModifiers.Add(hitBox.m_HitBoxModifier.Key, hitBox.m_HitBoxModifier.Value);
             }
-        }*/
+        }
+    }
+	
+	private void Update()
+    {
+        //move all this stuff to a targetable class.
+        Vector3 fishPosition = m_camera.WorldToViewportPoint(gameObject.transform.position);
+
+        //Checks if the position is in viewport.
+        bool onScreen = fishPosition.z > 0 && fishPosition.x > 0 && fishPosition.x < 1 && fishPosition.y > 0 && fishPosition.y < 1;
+
+        if (m_targetController == null)
+            return;
+
+        //adds and removes fish from a targetable list.
+        if (onScreen && !m_isListed)
+        {
+            m_targetController.m_fishInViewList.Add(gameObject);
+
+            m_isListed = true;
+        }
+        else if(!onScreen && m_isListed)
+        {
+            m_targetController.m_fishInViewList.Remove(gameObject);
+
+            m_isListed = false;
+        }
     }
 }
