9fecbac0f32383dddd16b3a86a135b2aae77412d
Grindead <samuel.paquette5@hotmail.com>
2019-11-18 14:49:30 -0500

    fish health bar

---
+++ Fist Fishing/Assets/Scripts/Fish/FishHealthBar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishHealthBar : MonoBehaviour
+{
+    public GameObject m_player;
+
+    //if health percentage == 1, isActive = false after a float m_delay.
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.LookAt(m_player.transform);
+    }
+}
