75f4d8b22902a15f1b08698ddcc8d9803d9caaad
EK <krus0021@algonquinlive.com>
2019-11-26 10:41:39 -0500

    DO NOT PULL - mid-work

---
+++ Fist Fishing/Assets/Scripts/UI/Menus/BasicMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BasicMenu : MonoBehaviour
+{
+
+    [SerializeField]
+    protected GameObject m_HUD;
+    public GameObject HUD { get { return m_HUD; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Fist Fishing/Assets/Scripts/UI/Menus/MenuManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Menus
+{
+    NotSet,
+    MainMenu,
+}
+
+[System.Serializable]
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    public InspectorDictionary<Menus, List<BasicMenu>> m_menuList = new InspectorDictionary<Menus, List<BasicMenu>> { };
+
+
+
+
+    #region singletonification
+    private static MenuManager Instance;
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject); //unity is stupid. Needs this to not implode
+        Instance = this;
+    }
+
+    private static void hasInstance()
+    {
+        if (Instance == default)
+            throw new System.InvalidOperationException("Menu Manager not Initilized");
+    }
+    #endregion singletonification
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
