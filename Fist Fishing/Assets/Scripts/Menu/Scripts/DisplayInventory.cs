2da379ce65290ce4f9dc62ee7b132fb792813557
Sam P <samuel.paquette5@hotmail.com>
2019-11-29 13:21:36 -0500

    inventory works better
    - Inventory doesn't have space between squares to accidentally delete items
    - Objects get deleted from menu when you run out of certain objects, example: crafting bait with spend all your Coral items.

--- Fist Fishing/Assets/Scripts/Menu/Scripts/DisplayInventory.cs
+++ Fist Fishing/Assets/Scripts/Menu/Scripts/DisplayInventory.cs
@@ -62,9 +62,15 @@ public class DisplayInventory : MonoBehaviour
         {
             if (slot.Value.m_ID >= 0)
             {
+                slot.Key.GetComponent<Image>().color = new Color(1, 1, 1, .5f);
                 //change sprite instead of changing color for future reference.
                 slot.Key.GetComponent<Image>().color = slot.Value.m_item.prefab.GetComponent<Image>().color;
-                slot.Key.GetComponentInChildren<TextMeshProUGUI>().text = slot.Value.m_amount.ToString("n0");
+                slot.Key.GetComponentInChildren<TextMeshProUGUI>().text = slot.Value.m_amount <= 0 ? "" : slot.Value.m_amount.ToString("n0");
+
+                if (slot.Value.m_amount <= 0)
+                {
+                    slot.Value.UpdateSlot(-1, null, 0, null);
+                }
             }
             else
             {
