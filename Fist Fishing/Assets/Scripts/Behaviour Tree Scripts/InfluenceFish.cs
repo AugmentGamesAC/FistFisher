41169ade663ad3749c9c98cce22d31c4bbfcea26
Brian <ande0246@algonquinlive.com>
2019-12-04 16:15:47 -0500

    Seperating out  influcenceFish
    made it it's own class, updated bait to use the code.

---
+++ Fist Fishing/Assets/Scripts/Behaviour Tree Scripts/InfluenceFish.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfluenceFish : MonoBehaviour
+{
+    [SerializeField]
+    protected FishBrain.FishClassification m_classification = FishBrain.FishClassification.BaitSensitive1;
+    [SerializeField]
+    protected float m_influence = 0.1f;
+    public void ImpressFish(GameObject other, float Influencefactor = 1)
+    {
+        FishBrain fb = other.GetComponentInParent<FishBrain>();
+        if (fb == default)
+            return;
+
+        fb.ApplyImpulse(gameObject, m_influence, m_classification);
+    }
+}
