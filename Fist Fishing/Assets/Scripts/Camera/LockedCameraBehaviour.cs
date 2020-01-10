c705c9257ebad529ee49ca1295009772d38f624d
Brian <ande0246@algonquinlive.com>
2020-01-10 07:29:15 -0500

    functionality and quick code cleanup
    simplification of behaviour for camera rotation, it's not good but it's function
    Changed ThirdPersonCamera to allow access and modification of LockedCamera's  settings
    Created orbit Point class for ease of use
    updated richard's scene as required.

--- Fist Fishing/Assets/Scripts/Camera/LockedCameraBehaviour.cs
+++ Fist Fishing/Assets/Scripts/Camera/LockedCameraBehaviour.cs
@@ -2,72 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class LockedCameraBehaviour : CameraBehaviour
 {
-
-    public GameObject m_lookAtObject;
-    public GameObject m_orbitObject;
-    float m_distance;
-    float m_yaw;
-    float m_yawInput;
-    float m_pitchInput;
-    float m_pitch;
-    float m_angleSpeed = 5.0f;
-    float m_maxVerticalAngle = 60.0f;
-    float m_offsetHeight = 3.0f;
+    [SerializeField]
+    protected OrbitPoint m_lookAtPoint;
+    [SerializeField]
+    protected OrbitPoint m_orbitPoint;
+    
     public override void UpdateRotation(float yawAmount, float pitchAmount)
     {
-        m_yawInput = yawAmount;
-        m_pitchInput = pitchAmount;
-    }
-
-    //Yaw and pitch should be the input multiplied by speed
-    public void IncrementRotation(float yawInput, float pitchInput)
-    {
-
-        if (yawInput == 0 || pitchInput == 0)
-        {
-            return;
-        }
-
-        //Set Pitch - Clamp Pitch to 89 degrees
-             = Mathf.Clamp(m_pitch - (pitchInput * m_angleSpeed), -m_maxVerticalAngle, m_maxVerticalAngle);
-        //Set Yaw
-
-        m_yaw += yawInput * m_angleSpeed;
-
-
-
-    }
-
-    public void SetOffset()
-    {
-        Vector3 offset = m_camera.transform.position - m_orbitObject.transform.position;
-
-
-        m_distance = Mathf.Clamp(offset.magnitude, 5, 7);
-
-        offset = Quaternion.Euler(m_pitch, m_yaw, 0.0f) * Vector3.forward;
-
-        //Bring camera out by distance given. this can be modified for different camera behaviours.
-
-        offset *= m_distance;
-
-        m_camera.transform.position = offset + m_orbitObject.transform.position;
-        m_camera.transform.position += m_camera.transform.up * m_offsetHeight;
-        m_lookAtObject.transform.position = -offset + m_orbitObject.transform.position;
+        m_orbitPoint.Increment(yawAmount, pitchAmount);
     }
 
     public override void UpdateCamera()
     {
-        IncrementRotation(ALInput.GetAxis(ALInput.AxisCode.MouseX), ALInput.GetAxis(ALInput.AxisCode.MouseY));
-        SetOffset();
-        m_camera.transform.LookAt(m_lookAtObject.transform);
-        m_orbitObject.transform.up = m_camera.transform.forward;
+        m_camera.transform.position = m_player.transform.position + m_orbitPoint.ReturnTargetPoint();
+
+        SetFacingDirection(m_player.transform.position + m_lookAtPoint.ReturnTargetPoint());
     }
 
-    public override void SetFacingDirection(Vector3 direction)
+    public override void SetFacingDirection(Vector3 goalPosition)
     {
-        throw new System.NotImplementedException();
+        m_camera.transform.LookAt(goalPosition, Vector3.up);
     }
 }
