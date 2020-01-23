ef947225262abfff5e50785390b4dc68d6e59703
Sam P <sam>
2020-01-23 17:30:05 -0500

    Upgrade and Stat management systems added
    - StatTracker added, like the health module but for any stat.
    - Statmanager added: takes care of list of stats and takes in modifications from upgrades.
    - Upgrade added: takes care of holding information to modify stats and cost for the shop.
    - UpgradeManager: Generates upgrades for the shop and updates prices on each upgrade.
    - Created a Combat folder in New Scripts.

---
+++ Fist Fishing/Assets/New Scripts/PlayerStatManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatManager
+{
+    /*enum Stats;
+    Dictionary<Stats, StatTracker> StatTrackerContainer;
+--
+AddStat();//for dictionary adding new objects.
+    UpdateStat(Stats trackerType, float amount);//change tracker values.
+--
+Responsibilities
+- keeps track of StatTrackers
+- fetch statTrackers with StatTrackerContainer[Stats]
+*/
+
+
+
+    public enum Stats
+    {
+        Health,
+        Location,
+        Stealth,
+        Power,
+        AirCapacity,
+        AirConsumption,
+        AirRestoration,
+        MovementSpeed,
+        TurnSpeed
+    }
+
+    
+    Dictionary<Stats, StatTracker> StatTrackerContainer;
+
+    /// <summary>
+    /// Sets StatTrackerContainer.
+    /// </summary>
+    public PlayerStatManager()
+    {
+        var ListOfStats = Enum.GetValues(typeof(Stats));
+        foreach (var stat in ListOfStats)
+        {
+            AddStat((Stats)stat, new StatTracker());
+        }
+    }
+
+    /// <summary>
+    /// Adds a KeyValuePair to the container.
+    /// </summary>
+    /// <param name="statType"></param>
+    /// <param name="statTracker"></param>
+    public void AddStat(Stats statType, StatTracker statTracker)
+    {
+        if (StatTrackerContainer.ContainsKey(statType))
+            return;
+
+        StatTrackerContainer.Add(statType, statTracker);
+    }
+
+    /// <summary>
+    /// add amount to the stat tracker fpr statType
+    /// </summary>
+    /// <param name="trackerType"></param>
+    /// <param name="amount"></param>
+    public bool UpdateStat(Stats statType, float amount)
+    {
+        if (!StatTrackerContainer.ContainsKey(statType))
+            return false;
+
+        StatTrackerContainer[statType].Change(amount);
+
+        return true;
+    }
+}
