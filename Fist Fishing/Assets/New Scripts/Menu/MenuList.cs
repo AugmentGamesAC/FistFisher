42a9bcad090dbdd648840b7c4f466214f78388db
Jason Allen <Jason94Allen@hotmail.com>
2020-01-24 13:49:00 -0500

    Work On New Menu stuff in NewScripts need help
    Still need to setup the canvas' and toggle them appropriately. Need help on understanding which lists to grab and how to do so using the Dictionary.

---
+++ Fist Fishing/Assets/New Scripts/Menu/MenuList.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuList : MonoBehaviour
+{
+    protected List<Menu> m_menuList;
+
+    bool m_pausesGameIfActive;
+    bool m_allowMouseToBeActive;
+
+    /// <summary>
+    /// Sets each item in the list to not show
+    /// Still need to setup the canvas' to make them show/not show
+    /// </summary>
+    /// <param name="activeState"></param>
+    public void ShowActive(bool activeState)
+    {
+        if (activeState)
+        {
+            m_pausesGameIfActive = true;
+            m_allowMouseToBeActive = true;
+
+            foreach (Menu item in m_menuList)
+            {
+                item.Show(true);
+            }
+        }
+        else
+        {
+            m_allowMouseToBeActive = false;
+            m_pausesGameIfActive = false;
+
+            foreach (Menu item in m_menuList)
+            {
+                item.Show(false);
+            }
+        }
+    }
+}
+
---
+++ Fist Fishing/Assets/New Scripts/Menu/NewMenuManager.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A list of all menu/HUD configurations, which are linked with a list of menus in the menu manager
+/// </summary>
+public enum MenuScreens
+{
+    /// <summary>
+    /// should do nothing
+    /// </summary>
+    NotSet,
+    /// <summary>
+    /// A way to start/exit game
+    /// </summary>
+    MainMenu,
+    /// <summary>
+    /// access to quit, options (which will be keybind menu access), and save/load/sound/difficulty/graphics in the far future 
+    /// </summary>
+    OptionsMenu,
+    /// <summary>
+    /// place to set keys for KBM input
+    /// </summary>
+    KeyboardControlsSetup,
+    /// <summary>
+    /// place to set keys for controller input
+    /// </summary>
+    ControllerControlsSetup,
+    /// <summary>
+    /// the screen showing the player inventory and the shop
+    /// </summary>
+    ShopMenu,
+    /// <summary>
+    /// basic player inventory
+    /// </summary>
+    SwimmingInventory,
+    /// <summary>
+    ///  small radial menu to allow the player to choose 1 of 4 preselected baits to be the active one
+    /// </summary>
+    QuickSelectPinwheel,
+    /// <summary>
+    /// anyting player normally sees
+    /// </summary>
+    NormalHUD,
+    /// <summary>
+    /// what player sees while boat is moving around the present scene
+    /// </summary>
+    BoatTravel,
+    /// <summary>
+    /// travel map for selecting other scenes to transport to
+    /// </summary>
+    LakeTravel,
+    /// <summary>
+    /// Options available when mounted on boat
+    /// </summary>
+    MountBoat,
+}
+
+/// <summary>
+/// singleton that handles all active menus
+/// </summary>
+[System.Serializable]
+public class NewMenuManager : MonoBehaviour
+{
+
+    #region working inspector dictionary
+    /// <summary>
+    /// this is the mess required to make dictionaries with  list as a value work in inspector
+    /// used in this case to pair enum of menu enum with a list of menu objects
+    /// </summary>
+    [System.Serializable]
+    public class MenuListForInspectorDictionary { public List<MenuList> m_listsOfMenu; }
+    [System.Serializable]
+    public class ListOfMenuConfigurations : InspectorDictionary<MenuScreens, MenuListForInspectorDictionary> { }
+    [SerializeField]
+    protected ListOfMenuConfigurations m_listOfConfigurations = new ListOfMenuConfigurations();
+    public ListOfMenuConfigurations MenuList { get { return m_listOfConfigurations; } }
+
+
+    [SerializeField]
+    public static MenuListForInspectorDictionary m_Mylist = new MenuListForInspectorDictionary();
+    #endregion working inspector dictionary
+
+
+
+    #region singletonification
+    /// <summary>
+    /// we only need one of these
+    /// </summary>
+    private static NewMenuManager Instance;
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject); //unity is stupid. Needs this to not implode
+        Instance = this;
+
+    }
+
+    private static void HasInstance()
+    {
+        if (Instance == default)
+            throw new System.InvalidOperationException("Menu Manager not Initilized");
+    }
+    #endregion singletonification
+
+
+
+
+    [SerializeField]
+    protected MenuScreens m_currentSelectedMenu = MenuScreens.NotSet;
+    /// <summary>
+    /// Decides which menus to display based on what screen we are currently on
+    /// Need help with this because im not sure how to get the MenuList's List of Menus
+    /// </summary>
+    /// <param name="currentlySelcetedMenuScreen"></param>
+    public static void DisplayMenuScreen(MenuScreens currentlySelcetedMenuScreen)
+    {
+        List<Menu> menuList = new List<Menu>();
+        if (Instance.m_currentSelectedMenu == currentlySelcetedMenuScreen)
+            return;
+
+        SetMenuListVisibility(default, false);
+        Instance.m_currentSelectedMenu = currentlySelcetedMenuScreen;
+
+        if (Instance.m_currentSelectedMenu == MenuScreens.NotSet)
+            return;
+        if (Instance.MenuList.TryGetValue(currentlySelcetedMenuScreen, out m_Mylist))
+        {
+            SetMenuListVisibility(m_Mylist,true);
+        }
+    }
+
+    protected static void SetMenuListVisibility(List<Menu> list, bool visibility)
+    {
+        // different code based on using list
+        if (m_Mylist == null)
+            return;
+        if (m_Mylist.m_listsOfMenu == null)
+            return;
+        if (list == null)
+            return;
+
+        //Why doesn't bm have a ShowHUD(bool)?
+        foreach (Menu bm in list)
+        // bm.HUD.SetActive(active);
+        {
+                bm.Show(visibility);
+           
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
