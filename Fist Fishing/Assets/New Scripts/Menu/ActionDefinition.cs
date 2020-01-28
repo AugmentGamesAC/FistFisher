cac7fb8f7c6d9d7e0630551f8d55032715845608
Eric Krushelniski <krus0021@algonquinlive.com>
2020-01-28 17:49:09 -0500

    Example Scene for Scrollable canvas with dynamically allocated options

---
+++ Fist Fishing/Assets/New Scripts/Menu/ActionDefinition.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// This is a class that contains the definition for each "action" (bait selection, atack selection, etc)
+/// 
+/// 
+/// 		internalId (enum or whatever), 
+/// 		HumanReadableId some string,
+/// 		detailed discription (yet another string), 
+/// 		ActionType (2axis,1axis,button,page), 
+/// 		ContextGroup as field (Boat Travel,inventoryshop,swiminventoy,Battle,Swimming,Menu Navigation)
+///
+/// </summary>
+public class ActionDefinition : MonoBehaviour
+{
+    public enum ActionID
+    {
+        BoatMovement,
+        PlayerMovement,
+        PlayerRotation,
+        CameraRotation,
+        DivingBellRaiseLower,
+        MountToggle, //merge mount toggle and gather?
+        Gather,
+        PageSelect, //which menu to focus on (and change shop tab)
+        HoldPick,
+        Action, //sell, buy, eat, attack
+        Flee,
+        Item, //bait
+        Targeting,
+        AttackSwap,
+        BaitSwap,
+
+        OpenShop,
+        OpenInventory,
+        OpenMainMenu,
+
+
+        InventoryAndMenuNavigation, // how does this tie in with ContextGroup.MenuNavigation?
+    }
+
+    [System.Flags]
+    public enum ContextGroup
+    {
+        BoatTravel = 0x0001,
+        InventoryShop = 0x0002,
+        SwimInventory = 0x0004,
+        Battle = 0x0010,
+        Swimming = 0x0020,
+        MenuNavigation = 0x0040,
+    }
+
+    [SerializeField]
+    protected ActionID m_internalID;
+    public ActionID InternalID => m_internalID;
+
+    public string m_humanReadableID;
+
+    public string m_detailedDescription;
+
+    [SerializeField]
+    protected ContextGroup m_contextGroups;
+    public ContextGroup ContextGroups => m_contextGroups;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Fist Fishing/Assets/New Scripts/Menu/ActionDefinitionListController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionDefinitionListController : MonoBehaviour
+{
+    [SerializeField]
+    protected List<GameObject> m_actionDefinitionPrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int count = 0;
+        foreach (GameObject AD in m_actionDefinitionPrefabs)
+        {
+            GameObject g = Instantiate(AD) as GameObject;
+            ActionDefinition def = g.GetComponent<ActionDefinition>();
+            if (def == default)
+            {
+                Debug.LogError("failed to instatniate " + g.name);
+            }
+            else
+            {
+                def.m_humanReadableID += count.ToString();
+                g.GetComponentInChildren<Text>().text = def.m_humanReadableID;
+
+                g.SetActive(true);
+                g.transform.SetParent(gameObject.transform, false);
+            }
+            count++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
