879bde5b366d8df31d5e39c7d793ccb8ba93d896
Jason Allen <Jason94Allen@hotmail.com>
2020-01-30 14:26:26 -0500

    Changed ImageUpdater to use RawImage types instead of Image types
    ImageUpdater: changed from Image to RawImage in order for updates to work as textures. Sprites would've worked if Image.sprite was still a thing.
    TestUIStateUpdateScene: deleted a prefab I made that shouldnt have been there. Added a RawImageToUpdate.

--- Fist Fishing/Assets/New Scripts/UI/ImageUpdater.cs
+++ Fist Fishing/Assets/New Scripts/UI/ImageUpdater.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class ImageUpdater : MonoBehaviour
@@ -8,12 +9,13 @@ public class ImageUpdater : MonoBehaviour
 
     [SerializeField]
     protected ImageTracker m_tracker;
-    protected Image m_image;
+    //Switched to a RawImage for texture use
+    protected RawImage m_image;
     
     // Start is called before the first frame update
     void Start()
     {
-        m_image = GetComponentInChildren<Image>();
+        m_image = GetComponentInChildren<RawImage>();
         m_tracker.OnStateChange += UpdateState;
     }
 
@@ -24,6 +26,9 @@ public class ImageUpdater : MonoBehaviour
         //Can't find a way to edit the image sprite or material due to it being a visual element need help
         //Could swap entire image with a seperate different image object
         //Image.image should work, but doesnt update during play or at all
-        m_image.image = value;
+
+        //RawImage texture swap works
+        m_image.texture = value;
+
     }
 }
