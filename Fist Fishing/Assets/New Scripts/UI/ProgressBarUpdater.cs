265e5800a16fc8915d93860e0183b4e052e55908
Jason Allen <Jason94Allen@hotmail.com>
2020-01-30 16:58:22 -0500

    Added Linear Progress Bar Updated By Tracker
    ProgressBarUpdater: Script that allows allows the user to change the bars max and min amounts as well as uses the FloatTracker to update its current fill amount.
    LinearProgressBarPrefab: prefab of the linear bar was created and placed in the UI folder of the root.

---
+++ Fist Fishing/Assets/New Scripts/UI/ProgressBarUpdater.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarUpdater : MonoBehaviour
+{
+    
+    [SerializeField]
+    protected int m_minFillAmount;
+    [SerializeField]
+    protected int m_maxFillAmount;
+    [SerializeField]
+    protected Image m_maskImage;
+    [SerializeField]
+    protected FloatTracker m_tracker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_tracker.OnStateChange += UpdateState;
+    }
+
+    protected void UpdateState(float fillValue)
+    {
+        float fillAmount = (fillValue - m_minFillAmount) / (m_maxFillAmount - m_minFillAmount);
+        m_maskImage.fillAmount = fillAmount;
+    }
+
+}
