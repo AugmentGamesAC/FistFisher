b9a67d317e5c6ec397631a601360488d77aa3349
Jason Allen <Jason94Allen@hotmail.com>
2020-01-30 17:45:37 -0500

    Added Radial Progress Bar Updated By Float Tracker
    ProgressBarUpdater: Changed script to grab the child object of "this" instead of having to place the fillable image into the script by hand
    RadialProgressBar: Created prefab for RadialProgressBar in UI/prefabs folder on root

--- Fist Fishing/Assets/New Scripts/UI/ProgressBarUpdater.cs
+++ Fist Fishing/Assets/New Scripts/UI/ProgressBarUpdater.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,20 +11,26 @@ public class ProgressBarUpdater : MonoBehaviour
     protected int m_minFillAmount;
     [SerializeField]
     protected int m_maxFillAmount;
-    [SerializeField]
-    protected Image m_maskImage;
+    protected Image m_fillImage;
+
     [SerializeField]
     protected FloatTracker m_tracker;
+   
     // Start is called before the first frame update
     void Start()
     {
+        //Get image with fill amount
+        m_fillImage = transform.GetChild(0).GetComponent<Image>();
+        //On a change occuring use UpdateState
         m_tracker.OnStateChange += UpdateState;
     }
 
     protected void UpdateState(float fillValue)
     {
+        //Use offsets set in editor to dynamically change bar fill size
         float fillAmount = (fillValue - m_minFillAmount) / (m_maxFillAmount - m_minFillAmount);
-        m_maskImage.fillAmount = fillAmount;
+        //Set the fill amount based on what is changed in the tracker
+        m_fillImage.fillAmount = fillAmount;
     }
 
 }
