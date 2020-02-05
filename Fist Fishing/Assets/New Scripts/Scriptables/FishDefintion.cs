2f420b1098d4c57f9bd21c3280142cb7281bb3cd
Brian Anderson <ande0246@algonquinlive.com>
2020-02-05 15:20:57 -0500

    DO NOT MERGE- INPROGRESS
    Creation of new scripting Pattern for inventory

---
+++ Fist Fishing/Assets/New Scripts/Scriptables/FishDefintion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishDefintion : ScriptableObject, IFishData, IItem
+{
+    [SerializeField]
+    protected float m_damage;
+    public float Damage => m_damage;
+
+    [SerializeField]
+    protected float m_combatSpeed;
+    public float CombatSpeed => m_combatSpeed;
+    [SerializeField]
+    protected float m_attackRange;
+    public float AttackRange => m_attackRange;
+
+    [SerializeField]
+    protected FishHealth m_Health;
+    public FishHealth Health => m_Health;
+
+
+    public FishBrain.FishClassification FishClassification => throw new System.NotImplementedException();
+
+    public int StackSize => throw new System.NotImplementedException();
+
+    public int ID => throw new System.NotImplementedException();
+
+    public int WorthInCurrency => throw new System.NotImplementedException();
+
+    public ItemType Type => throw new System.NotImplementedException();
+
+    public string Description => throw new System.NotImplementedException();
+
+
+
+    public Sprite Sprite => throw new System.NotImplementedException();
+    public Sprite IconDisplay => throw new System.NotImplementedException();
+}
