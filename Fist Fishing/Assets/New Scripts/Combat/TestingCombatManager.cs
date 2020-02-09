80d05cb48ff28ee393fe63442bd4776b0b58e5dd
Brian <ande0246@algoquinlive.com>
2020-02-09 16:28:44 -0500

    setting up combat manager to work correctly needs merge with SAm's work
    new fish created

--- Fist Fishing/Assets/New Scripts/Combat/TestingCombatManager.cs
+++ Fist Fishing/Assets/New Scripts/Combat/TestingCombatManager.cs
@@ -12,7 +12,11 @@ using System.Linq;
 public class TestingCombatManager : CombatManager
 {
     [SerializeField]
-    protected FishDefintion m_fish;
+    protected FishDefintion m_f5Fish;
+    [SerializeField]
+    protected FishDefintion m_f6Fish;
+    [SerializeField]
+    protected FishDefintion m_f7Fish;
     [SerializeField]
     protected SelectedFishUI m_showyFish;
 
@@ -27,30 +31,55 @@ public class TestingCombatManager : CombatManager
 
         m_playerCombatInfo.m_attackPinwheel = new PinWheel<CombatMoveInfo>(1, moves);
         m_playerCombatInfo.m_attackPinwheel.SetSelectedOption(1);
+
+        m_showyFish.UpdateUI(default);
     }
 
-    [ContextMenu("Create Test Fish")]
-    public void newFish()
+    [ContextMenu("CombatYeast/Player Started F5 Fish battle")]
+    public void newFishF5True() { StartCombatTest(new []{m_f5Fish}, true); }
+    [ContextMenu("CombatYeast/Player Started F6 Fish battle")]
+    public void newFishF6True() { StartCombatTest(new[] { m_f6Fish }, true); }
+    [ContextMenu("CombatYeast/Player Started F7 Fish battle")]
+    public void newFishF7True() { StartCombatTest(new[] { m_f7Fish }, true); }
+    [ContextMenu("CombatYeast/Fish Started F5 Fish battle")]
+    public void newFishF5False() { StartCombatTest(new[] { m_f5Fish }, false); }
+    [ContextMenu("CombatYeast/Fish Started F6 Fish battle")]
+    public void newFishF6False() { StartCombatTest(new[] { m_f6Fish }, false); }
+    [ContextMenu("CombatYeast/Fish Started F7 Fish battle")]
+    public void newFishF7False() { StartCombatTest(new[] { m_f7Fish }, false); }
+
+    protected void StartCombatTest(IEnumerable<FishDefintion> fishDefs, bool wasPlayer)
     {
-        //Create new fish data
-        FishCombatInfo NewFish = new FishCombatInfo(new FishInstance(m_fish));
-        m_FishSelection.AddItem(NewFish);
-        m_showyFish.UpdateUI(NewFish);
-        m_roundQueue.Enqueue(NewFish);
+        NewMenuManager.DisplayMenuScreen(MenuScreens.Combat);
+        var fishies = fishDefs.Select(X => new FishCombatInfo(new FishInstance(X)));
+        m_showyFish.UpdateUI(fishies.FirstOrDefault());
+        foreach (var fish in fishies)
+            m_FishSelection.AddItem(fish);
+        base.StartCombat(wasPlayer);
     }
+
 
     public new void Update()
     {
         base.Update();
 
+        if (ALInput.GetKeyDown(KeyCode.F5))
+            newFishF5True();
+        if (ALInput.GetKeyDown(KeyCode.F6))
+            newFishF6True();
+        if (ALInput.GetKeyDown(KeyCode.F7))
+            newFishF7True();
+        if (ALInput.GetKeyDown(KeyCode.F9))
+            newFishF5False();
+        if (ALInput.GetKeyDown(KeyCode.F10))
+            newFishF6False();
+        if (ALInput.GetKeyDown(KeyCode.F11))
+            newFishF7False();
+
+
         if (NewMenuManager.CurrentMenu != MenuScreens.Combat)
             return;
 
-        if (ALInput.GetKeyDown(ALInput.Start))
-        {
-            newFish();
-            StartCombat(true);
-        }
         //if ((ALInput.GetKeyDown(ALInput.Punch)))
         //    PlayerAttack();
     }
