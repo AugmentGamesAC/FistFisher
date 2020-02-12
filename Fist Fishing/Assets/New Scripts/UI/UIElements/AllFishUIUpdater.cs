3bce537d2df7ff5caab8b77396467a03d08c0fd0
Brian <ande0246@algonquinlive.com>
2020-02-12 16:23:31 -0500

    QuickChange to add in new AllFishUpdaterIdeas

---
+++ Fist Fishing/Assets/New Scripts/UI/UIElements/AllFishUIUpdater.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CoreUIElement<FishCombatInfo>))]
+public class AllFishUIUpdater : CoreUIUpdater<SingleSelectionListTracker<FishCombatInfo>, CoreUIElement<FishCombatInfo>, ISingleSelectionList<FishCombatInfo>>
+{
+    [SerializeField]
+    protected List<SelectedFishUI> m_unselectedFish = new List<SelectedFishUI>();
+
+    protected override void UpdateState(ISingleSelectionList<FishCombatInfo> value)
+    {
+        if (value.Count < 1)
+        {
+            ClearUI();
+            return;
+        }
+
+        m_UIElement.UpdateUI(value.SelectedItem);
+
+        int targetIndex = 0;
+        foreach (var unselected in m_unselectedFish)
+        {
+            if (targetIndex == value.Selection)
+                targetIndex++;
+
+            unselected.UpdateUI(targetIndex >= value.Count ? default : value[targetIndex]);
+            targetIndex++;
+        }
+    }
+
+    protected void ClearUI()
+    {
+        m_UIElement.UpdateUI(default);
+        foreach (var unselected in m_unselectedFish)
+            unselected.UpdateUI(default);
+    }
+}
+
