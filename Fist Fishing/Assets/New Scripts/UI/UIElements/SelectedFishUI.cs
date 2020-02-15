b555f5ecc670cf3a5aa1177a2854ce278193f145
Brian <ande0246@algonquinlive.com>
2020-02-14 20:38:57 -0500

    quick change to displays to show 0

--- Fist Fishing/Assets/New Scripts/UI/UIElements/SelectedFishUI.cs
+++ Fist Fishing/Assets/New Scripts/UI/UIElements/SelectedFishUI.cs
@@ -36,8 +36,11 @@ public class SelectedFishUI : CoreUIElement<FishCombatInfo>
         EnemyNameDisplay.ForceUpdate(newData.FishInstance.FishData.Item.Name);
         EnemyIconDisplay.ForceUpdate(newData.FishInstance.FishData.IconDisplay);
         //EnemyAction.ForceUpdate(newData.FishInstance.FishData.IconDisplay);
-        EnemyAction.ForceUpdate(0);
-        EnemyTypeDisplay.ForceUpdate( (int)Mathf.Log(2,(float) (newData.FishInstance.FishData.FishClassification & (FishBrain.FishClassification.Agressive|FishBrain.FishClassification.Fearful|FishBrain.FishClassification.Passive))) );
+        EnemyAction.ForceUpdate(newData.FishInstance.FishData.FishClassification.HasFlag(FishBrain.FishClassification.Agressive)?1:0 );
+        EnemyTypeDisplay.ForceUpdate(
+            newData.FishInstance.FishData.FishClassification.HasFlag(FishBrain.FishClassification.Passive) ? 0 :
+                        newData.FishInstance.FishData.FishClassification.HasFlag(FishBrain.FishClassification.Agressive) ? 1 : 2);
+
         EnemyHealthNumberDisplay.UpdateTracker(newData.FishInstance.Health.PercentTracker);
         EnemySwimSpeedDisplay.UpdateTracker(newData.Speed);
     }
