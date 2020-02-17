c12075c223f19b095d40c03935a7915c50587efc
Jason <Jason94Allen@hotmail.com>
2020-02-17 16:33:47 -0500

    Created ScriptableObject for BaitData
    - New folder in NewScripts called Items
    - Bait_IItem: Basic script to hold editable data when creating a new Bait item as an asset
    - Added 3 new elements to the enum BaitSensitive1 - 4

---
+++ Fist Fishing/Assets/New Scripts/Items/Bait_IItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BaitData", menuName = "ScriptableObjects/Bait", order = 1)]
+public class Bait_IItem : ScriptableObject, IItem
+{
+  [SerializeField]
+    protected int m_stackSize;
+    public int StackSize => m_stackSize;
+    [SerializeField]
+    protected int m_id;
+    [SerializeField]
+    public int ID => m_id;
+    [SerializeField]
+    protected int m_worth;
+    public int WorthInCurrency => m_worth;
+    [SerializeField]
+    protected ItemType m_type;
+    public ItemType Type => m_type;
+    [SerializeField]
+    protected string m_description;
+    public string Description => m_description;
+    [SerializeField]
+    protected Sprite m_display;
+    public Sprite IconDisplay => m_display;
+    [SerializeField]
+    protected string m_name;
+    public string Name => m_name;
+    [SerializeField]
+    protected FishBrain.FishClassification m_currentBaitType = FishBrain.FishClassification.BaitSensitive1;
+
+
+}
