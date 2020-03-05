2e000522a090e35c6a7641f271243e2ae24fc45a
EK <erickrushelniski@gmail.com>
2020-03-05 13:07:15 -0500

    DNP - Brian changes before moving computers

---
+++ Fist Fishing/Assets/Editor/ZoneStamper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BiomeInstance))]
+public class ZoneStamper : MonoBehaviour
+{
+    public string NewBiomeName;
+    protected BiomeInstance m_biomeInstance;
+    
+    [ContextMenu("DuplicateAndNameBiome")]
+    public void CloneBiome()
+    {
+
+    }
+
+    [ContextMenu("DuplicateAndShareBiome")]
+    public void CloneBiome()
+    {
+
+    }
+}
